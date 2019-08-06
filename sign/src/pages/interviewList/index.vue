7bb0eb3bf4fba780fcd906de05fcaca6845f2ce3
zwj <2317133489@qq.com>
2019-08-06 19:42:59 +0800

    构建

---
+++ sign/src/pages/interviewList/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="list-warp">
+    <div>sds</div>
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store'
+
+export default {
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style>
+.list-warp{
+  width:100%;
+  height: 100rpx;
+  background: red;
+}
+</style>
