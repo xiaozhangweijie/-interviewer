ee9a4937aade7c990558efec3e497b913d27142a
zwj <2317133489@qq.com>
2019-08-07 10:24:24 +0800

    map

--- sign/src/pages/index/index.vue
+++ sign/src/pages/index/index.vue
@@ -1,30 +1,49 @@
 <template>
   <div class="wrap">
-<section class="section"></section>
+<section class="section">
+    <map
+      id="map"
+      show-location
+      :markers="markers"
+      :longitude="longitude"
+      :latitude="latitude"
+    ></map>
+      <cover-view class="location" @click="location">
+       <button>定位</button>
+    </cover-view>
+</section>
     <footer class="footer" @click="look">
       添加面试
     </footer>
   </div>
 </template>
 <script>
-
+import {mapState, mapActions} from 'vuex';
 export default {
   data () {
     return {
-   
+   markers:[]
     }
   },
 
   components: {
    
   },
-
+computed:{
+  ...mapState({
+      longitude: state=>state.home.longitude,
+      latitude: state=>state.home.latitude
+    })
+},
   methods: {
    look(){
      wx.navigateTo({
        url:"/pages/AddInterviews/main"
      })
-   }
+   },
+       ...mapActions({
+      location: 'home/getLocation'
+    })
   },
 
   created () {
@@ -32,7 +51,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 *{
   margin:0;
   padding:0;
@@ -45,6 +64,19 @@ html,body{
 .wrap{
   width:100%;
   height:100%;
+}
+.section{
+  width:100%;
+  height:553px;
+ map{
+    width:100%;
+    height:100%;
+ }
+ .location{
+  position: fixed;
+  bottom: 100rpx;
+  left: 30rpx;
+}
 }
 .footer{
   width:100%;
